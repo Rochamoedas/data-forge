f704979b2d922370cbaea5a4615b39ca4c6ba7ce
unknown <rochanft@gmail.com>
2025-06-08 03:08:06 -0300

    Changed API limits

--- app/config/api_limits.py
+++ app/config/api_limits.py
@@ -4,17 +4,17 @@ class APILimits(BaseModel):
     """Configuration for API limits and constraints"""
     
     # Pagination limits
-    DEFAULT_PAGE_SIZE: int = 10
+    DEFAULT_PAGE_SIZE: int = 999
     MAX_PAGE_SIZE: int = 10000
     MIN_PAGE_SIZE: int = 1
     
     # Stream limits
-    DEFAULT_STREAM_LIMIT: int = 100
-    MAX_STREAM_LIMIT: int = 10000
+    DEFAULT_STREAM_LIMIT: int = 10000
+    MAX_STREAM_LIMIT: int = 100001
     MIN_STREAM_LIMIT: int = 1
     
     # Bulk operation limits
-    MAX_BULK_RECORDS: int = 1000
+    MAX_BULK_RECORDS: int = 100001
     MIN_BULK_RECORDS: int = 1
     
     # Query limits
@@ -23,7 +23,7 @@ class APILimits(BaseModel):
     
     # Performance limits
     QUERY_TIMEOUT_SECONDS: int = 30
-    MAX_CONCURRENT_STREAMS: int = 10
+    MAX_CONCURRENT_STREAMS: int = 5
 
 # Global instance
 api_limits = APILimits() 
